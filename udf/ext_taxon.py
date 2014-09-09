dd821026e330cf521fd94a636e8f758ccd27ba4b
zhangce <czhang@cs.wisc.edu>
2014-09-09 16:37:16 -0500

    more comments

--- udf/ext_taxon.py
+++ udf/ext_taxon.py
@@ -3,8 +3,8 @@ import json, re, sys, os
 
 BASE_FOLDER = os.path.split(os.path.realpath(__file__))[0]
 
-dict_fossils = {}
-dict_species_lastword = {}
+dict_fossils = {} # This dictionary contains a map from taxon name to rank
+dict_species_lastword = {} # This dictionary contains a set of valid species name
 
 for l in open(BASE_FOLDER + '/../dict/treatise_measurements.txt').read().splitlines():
   ss = l.split('\t')
@@ -45,36 +45,40 @@ for l in open(BASE_FOLDER + '/../dict/paleodb_taxons.tsv'):
     continue
 
 # For each input tuple
-cache = {}
+cache = {} # the cache of (nearst) genus
 for row in sys.stdin:
   sent = json.loads(row)
   docid = sent["sentence.docid"]
   sentid = sent["sentence.sentid"]
   words = sent["sentence.words"]
 
-  prevword = ""
-  id = 1
-  for w in words:
-    w2 = re.sub('[^a-zA-Z]', '', w)
-    if w2 != '' and w2.lower() in dict_fossils and dict_fossils[w2.lower()] == 'genus':
-      cache[w2.lower()[0]] = w2.lower()
+  prevword = "" # The previous word in the sentence when enumerating words
+  id = 1 # Curernt word ID while enumerating words
+  for w in words: ## For each word
+    w2 = re.sub('[^a-zA-Z]', '', w) # remove non english words (most OCR errors)
+    if w2 != '' and w2.lower() in dict_fossils and dict_fossils[w2.lower()] == 'genus': # if this word is a genus
+      cache[w2.lower()[0]] = w2.lower() # udpate the cache of (nearst) genus (P -> Pxxxxxxx)
 
-    egenus = ""
-    if w.lower() in dict_species_lastword:
-      if prevword.lower() in dict_fossils and prevword != "": ## How to fix P # and len(prevword) > 3:
+    egenus = "" # result of searching for taxons. It will be != "" if we find a genus
+    if w.lower() in dict_species_lastword: # if the current word is a valid species name
+      if prevword.lower() in dict_fossils and prevword != "": # If the previous word is a higher-rank taxon
         egenus = prevword + ' ' + w
         egenus = re.sub('[^a-zA-Z ]', '' , egenus.encode('ascii', 'ignore'))
-      elif prevword != "":
+      elif prevword != "": # Check cases for abbrv. 
         prev2 = re.sub('[^a-zA-Z0-9\.]', '', prevword)
         if re.search('^[A-Z]\.?$', prev2):
           if prev2[0].lower() in cache:
             egenus = cache[prev2[0].lower()] + ' ' + w.lower()
             egenus = re.sub('[^a-zA-Z ]', '' , egenus.encode('ascii', 'ignore'))
 
-    if egenus != "":
+    if egenus != "": # If we found a genus, output a mention
       mid = "TAXON_" + sentid + "_%d_%d" % (id-1, id)
       print json.dumps({"docid":docid, "mid":mid, "word":egenus, "type":"TAXON"})
 
     prevword = w
     id = id + 1
+
+
+
+
 
